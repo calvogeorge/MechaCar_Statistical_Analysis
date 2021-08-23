e5b5c71a89fd8f37dc75a6c56722bb2be7f0891c
calvogeorge <calvogeorge@hotmail.com>
2021-08-22 20:24:04 -0400

    Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,23 @@
 library(dplyr)
-MechaCar_mpg <- read.csv("MechaCar_mpg.csv", stringsAsFactors = F)
+library(tidyverse)
+
+#Deliverable 1
+MechaCar_mpg <- read.csv("MechaCar_mpg.csv", stringsAsFactors = F) #
 
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar_mpg))
+
+# Deliverable 2
+
+Suspension_Coil_Table <- read.csv("Suspension_Coil.csv", stringsAsFactors = F)
+
+total_summary = Suspension_Coil_Table %>% 
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups =  'keep')
+
+lot_summary = Suspension_Coil_Table %>%
+  group_by(Manufacturing_Lot) %>%
+  summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups =  'keep')
+
+# Deliverable 3
+  
