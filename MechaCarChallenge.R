801960f78e2010d2b5ef3b6fd8dbd5c73d90ccf1
calvogeorge <calvogeorge@hotmail.com>
2021-08-22 20:52:38 -0400

    Deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -20,4 +20,27 @@ lot_summary = Suspension_Coil_Table %>%
   summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI), SD=sd(PSI), .groups =  'keep')
 
 # Deliverable 3
-  
+
+Sample_Suspension_Coil <- Suspension_Coil_Table %>%
+  sample_n(50) # Generate 50 random sample table
+
+t.test((Sample_Suspension_Coil$PSI),mu=mean(Suspension_Coil_Table$PSI))
+
+# Creating sub
+
+lot1_Suspension_Coil <- Suspension_Coil_Table %>%
+  filter(Manufacturing_Lot == 'Lot1')
+
+lot2_Suspension_Coil <- Suspension_Coil_Table %>%
+  filter(Manufacturing_Lot == 'Lot2')
+
+lot3_Suspension_Coil <- Suspension_Coil_Table %>%
+  filter(Manufacturing_Lot == 'Lot3')
+
+# T.Test Per Lot against total population
+
+t.test((lot1_Suspension_Coil$PSI),mu=mean(Suspension_Coil_Table$PSI))
+
+t.test((lot2_Suspension_Coil$PSI),mu=mean(Suspension_Coil_Table$PSI))
+
+t.test((lot3_Suspension_Coil$PSI),mu=mean(Suspension_Coil_Table$PSI))
